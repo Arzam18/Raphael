7575d29e5232ee3d39670cd6f53c69eeeb48cc22
Arzam18 <98748640+Arzam18@users.noreply.github.com>
2026-09-20 13:27:01 +0800

    Update sparse.cpp

--- src/eval/sparse.cpp
+++ src/eval/sparse.cpp
@@ -1,42 +1,116 @@
-#ifdef EVAL_NNUE
+#ifdef EVAL_MULTILAYER
 
 #include <eval/sparse.h>
 
+using namespace raphael::nnue;
+using std::popcount;
+
 #ifdef USE_SIMD
 
-namespace raphael::nnue::sparse {
+void SparseIterator::add_nonzeros(VecU8 l0_out0, VecU8 l0_out1) {
 
-namespace {
+    constexpr i32 regw32 = ALIGNMENT / sizeof(i32);
 
-alignas(ALIGNMENT) static constexpr u16 nonzero_idx[256][8] = [] {
-    u16 table[256][8]{};
+#ifdef USE_AVX512
 
-    for (u32 mask = 0; mask < 256; ++mask) {
-        u32 count = 0;
+    static_assert(USE_SIMD == 512);
 
-        for (u32 i = 0; i < 8; ++i) {
-            if (mask & (1u << i))
-                table[mask][count++] = static_cast<u16>(i);
-        }
+    const auto mask =
+        _mm512_kunpackw(
+            nonzero_mask(l0_out1),
+            nonzero_mask(l0_out0)
+        );
+
+    const auto idxs =
+        _mm512_maskz_compress_epi16(mask, offset_);
+
+    _mm512_storeu_si512(&indices_[count_], idxs);
+
+    offset_ = add_i16(offset_, full_i16(32));
+
+    count_ += popcount(mask);
+
+#elif defined(USE_NEON)
+
+    static_assert(USE_SIMD == 128);
+    static_assert(ALIGNMENT == 16);
+    static_assert(regw32 == 4);
+
+    const u32 full_mask =
+        (nonzero_mask(l0_out1) << regw32)
+        | nonzero_mask(l0_out0);
+
+    const u8 mask = static_cast<u8>(full_mask & 0xFF);
+
+    const auto idxs =
+        add_i16(
+            offset_,
+            load_i16(
+                reinterpret_cast<const i16*>(&nonzero_idx[mask])
+            )
+        );
+
+    store_i16(&indices_[count_], idxs);
+
+    offset_ = add_i16(
+        offset_,
+        full_i16(8)
+    );
+
+    count_ += popcount(mask);
+
+#else
+
+    static_assert(regw32 % 8 == 0);
+
+    u32 full_mask =
+        (nonzero_mask(l0_out1) << regw32)
+        | nonzero_mask(l0_out0);
+
+    for (i32 i = 0; i < regw32 / 4; i++) {
+
+        // get offset of up to 8 nonzeros at a time
+        const u8 mask = full_mask & 0xFF;
+
+        full_mask >>= 8;
+
+        const auto idxs =
+            _mm_add_epi16(
+                offset_,
+                _mm_load_si128(
+                    reinterpret_cast<const __m128i*>(
+                        &nonzero_idx[mask]
+                    )
+                )
+            );
+
+        _mm_storeu_si128(
+            reinterpret_cast<__m128i*>(&indices_[count_]),
+            idxs
+        );
+
+        offset_ =
+            _mm_add_epi16(
+                offset_,
+                _mm_set1_epi16(8)
+            );
+
+        count_ += popcount(mask);
     }
 
-    return table;
-}();
+#endif
 
-}  // namespace
-
-
-SparseInput SparseInput::load(const void* src) {
-    return {load_i16(src)};
+    assert(count_ <= L1_SIZE / 4);
 }
 
-
-void SparseInput::store_into(void* dst) const {
-    store_i16(dst, offset_);
+i32 SparseIterator::count() const {
+    return count_;
 }
 
-
-}  // namespace raphael::nnue::sparse
+i32 SparseIterator::index(i32 nnz_id) const {
+    assert(nnz_id < count_);
+    return indices_[nnz_id];
+}
 
 #endif
 #endif
