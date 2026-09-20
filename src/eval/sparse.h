e7cd5027e9d0730e3aea13d079be59b542d2608c
Arzam18 <98748640+Arzam18@users.noreply.github.com>
2026-09-20 13:25:59 +0800

    Update sparse.h

--- src/eval/sparse.h
+++ src/eval/sparse.h
@@ -1,24 +1,82 @@
 #pragma once
-
-#ifdef EVAL_NNUE
-
+#ifdef EVAL_MULTILAYER
+#include <eval/arch.h>
 #include <eval/simd.h>
+
+namespace raphael::nnue {
+
+class SparseIterator {
 
 #ifdef USE_SIMD
 
-namespace raphael::nnue::sparse {
+private:
+    u16 indices_[L1_SIZE / 4] = {};
+    i32 count_ = 0;
 
-struct SparseInput {
-    VecI16 offset_;
+#ifdef USE_AVX512
 
-    [[nodiscard]] static SparseInput load(
-        const void* src
+    // clang-format off
+    __m512i offset_ = _mm512_set_epi16(
+        31, 30, 29, 28, 27, 26, 25, 24,
+        23, 22, 21, 20, 19, 18, 17, 16,
+        15, 14, 13, 12, 11, 10,  9,  8,
+         7,  6,  5,  4,  3,  2,  1,  0
     );
+    // clang-format on
 
-    void store_into(void* dst) const;
-};
+#elif defined(USE_NEON)
 
-}  // namespace raphael::nnue::sparse
+    VecI16 offset_ = zero_i16();
+
+#else
+
+    __m128i offset_ = _mm_setzero_si128();
 
 #endif
+
+#if !defined(USE_AVX512)
+
+    // precompute nonzero_idx[mask][nnz_idx] = position of nonzero block
+    alignas(16) static constexpr MultiArray<u16, 256, 8> nonzero_idx = [] {
+        MultiArray<u16, 256, 8> idx{};
+
+        for (i32 i = 0; i < 256; i++) {
+            i32 nnz = 0;
+
+            for (u8 mask = i; mask != 0; mask &= mask - 1)
+                idx[i][nnz++] = std::countr_zero(mask);
+        }
+
+        return idx;
+    }();
+
+#endif
+
+public:
+
+    /** Adds the nonzero indices to the sparse iterator
+     *
+     * \param l0_out0 first chunk of l0 outputs
+     * \param l0_out1 second chunk of l0 outputs
+     */
+    void add_nonzeros(VecU8 l0_out0, VecU8 l0_out1);
+
+    /** Returns the number of nonzero blocks
+     *
+     * \returns the nnz count
+     */
+    [[nodiscard]] i32 count() const;
+
+    /** Returns the tile id for a nonzero block
+     *
+     * \param nnz_id which nonzero chunk we want the index for
+     * \returns the corresponding tile id
+     */
+    [[nodiscard]] i32 index(i32 nnz_id) const;
+
+#endif
+
+};
+
+}  // namespace raphael::nnue
 #endif
